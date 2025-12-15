05eda29113a3f3659ea5fe44066e840549be2594
zengwangcheng <38219893+greenhand0403@users.noreply.github.com>
2025-12-15 17:45:25 +0800

    day16陀螺仪i2c通讯超时重启

--- PT32Y003x_StdPeriph_Examples/13_PT32Y003x_Gamepad/gyro_driver.c
+++ PT32Y003x_StdPeriph_Examples/13_PT32Y003x_Gamepad/gyro_driver.c
@@ -1,6 +1,17 @@
 // gyro_driver.c
 #include "gyro_driver.h"
 #include "delay.h"
+// 返回1=等到了，0=超时
+static uint8_t i2c_wait_flag_set(uint32_t flag, uint32_t timeout_ms)
+{
+    uint32_t t0 = s_ms_ticks; // 你的1ms计数（volatile）
+    while (I2C_GetFlagStatus(I2C0, flag) != SET) {
+        if ((uint32_t)(s_ms_ticks - t0) >= timeout_ms) {
+            return 0;
+        }
+    }
+    return 1;
+}
 
 void gyro_init(void)
 {
@@ -17,40 +28,49 @@ void gyro_init(void)
 
     I2C_Cmd(I2C0, ENABLE);
 }
+static void i2c_recover(void)
+{
+    I2C_Cmd(I2C0, DISABLE);
+    // 可选：延时几百us
+    I2C_Cmd(I2C0, ENABLE);
+}
 void I2C_EE_Read(uint8_t* pBuffer,uint16_t ReadAddr, uint16_t DeviceAddr, uint16_t data_size)
 {
 	int i;
 /******************等待从机ready***************/		
 	I2C_GenerateEvent(I2C0,I2C_Event_Start,ENABLE);
-	while(I2C_GetFlagStatus(I2C0,I2C_FLAG_StartOk)!= SET);
+	if (!i2c_wait_flag_set(I2C_FLAG_StartOk, 5)) goto fail;
 	I2C_SendAddr(I2C0, DeviceAddr);//器件地址，写
-	while(I2C_GetFlagStatus(I2C0,I2C_FLAG_MASGetAckW)!=SET);
+	if (!i2c_wait_flag_set(I2C_FLAG_MASGetAckW, 5)) goto fail;
 	I2C_SendData(I2C0,ReadAddr);//发送要读的页地址
-	while(I2C_GetFlagStatus(I2C0,I2C_FLAG_MDSGetAck)!=SET);
+	if (!i2c_wait_flag_set(I2C_FLAG_MDSGetAck, 5)) goto fail;
 	I2C_GenerateEvent(I2C0,I2C_Event_Stop,ENABLE);
 /******************接收数据***************/		
 	I2C_GenerateEvent(I2C0,I2C_Event_Start,ENABLE);
-	while(I2C_GetFlagStatus(I2C0,I2C_FLAG_StartOk)!= SET);
+	if (!i2c_wait_flag_set(I2C_FLAG_StartOk, 5)) goto fail;
 	I2C_SendAddr(I2C0, DeviceAddr|0x1);//器件地址，读
-	while(I2C_GetFlagStatus(I2C0,I2C_FLAG_MASGetAckR)!=SET);
+	if (!i2c_wait_flag_set(I2C_FLAG_MASGetAckR, 5)) goto fail;
 	I2C0->CR=I2C_CR_ACK;
 	for(i=0;i<data_size;i++)
 	{
 		if(i==(data_size-1))
 		{
 			I2C0->CCR=I2C_CCR_ACK|I2C_CCR_SI;//主机发送NACK
-			while(I2C_GetFlagStatus(I2C0,I2C_FLAG_MDGSendNack) != SET);
+			if (!i2c_wait_flag_set(I2C_FLAG_MDGSendNack, 5)) goto fail;
 			*pBuffer=I2C_ReceiveData(I2C0);
 			pBuffer++;
 			break;
 		}
 		I2C0->CCR=I2C_CCR_SI;
-		while(I2C_GetFlagStatus(I2C0,I2C_FLAG_MDGSendAck) != SET);
+		if (!i2c_wait_flag_set(I2C_FLAG_MDGSendAck, 5)) goto fail;
 		*pBuffer=I2C_ReceiveData(I2C0);
 		pBuffer++;
 	}
 	/******************发送停止位***************/
 	I2C_GenerateEvent(I2C0,I2C_Event_Stop,ENABLE);
+    return;          // ★ 成功直接返回
+    fail:
+    i2c_recover();
 }
 void I2C_EE_Write(uint8_t* pBuffer, unsigned int WriteAddr,uint16_t DeviceAddr, uint16_t data_size)
 {
@@ -61,18 +81,21 @@ void I2C_EE_Write(uint8_t* pBuffer, unsigned int WriteAddr,uint16_t DeviceAddr, 
 	I2C_Cmd(I2C0,DISABLE);
 	I2C_Cmd(I2C0,ENABLE);
 	I2C_GenerateEvent(I2C0,I2C_Event_Start,ENABLE);	
-	while(I2C_GetFlagStatus(I2C0,I2C_FLAG_StartOk)!= SET);
+		if (!i2c_wait_flag_set(I2C_FLAG_StartOk, 5)) goto fail;
 	I2C_SendAddr(I2C0, DeviceAddr);//器件地址，写
-	while(I2C_GetFlagStatus(I2C0,I2C_FLAG_MASGetAckW)!=SET);
+	if (!i2c_wait_flag_set(I2C_FLAG_MASGetAckW, 5)) goto fail;
 	I2C_SendData(I2C0,WriteAddr);//发送要写的字地址
-	while(I2C_GetFlagStatus(I2C0,I2C_FLAG_MDSGetAck)!=SET);
+	if (!i2c_wait_flag_set(I2C_FLAG_MDSGetAck, 5)) goto fail;
 	for(i=0;i<data_size;i++)
 	{
 		I2C_SendData(I2C0, *(pBuffer++));
-		while(I2C_GetFlagStatus(I2C0,I2C_FLAG_MDSGetAck) != SET);
+		if (!i2c_wait_flag_set(I2C_FLAG_MDSGetAck, 5)) goto fail;
 	}
 	/******************发送停止位***************/
 	I2C_GenerateEvent(I2C0,I2C_Event_Stop,ENABLE);
+    return;          // ★ 成功直接返回
+    fail:
+    i2c_recover();
 }
 uint8_t MPU_ReadReg(uint8_t reg)
 {
@@ -94,10 +117,11 @@ int16_t be16_to_i16(uint8_t hi, uint8_t lo)
 }
 void gyro_first_read(void)
 {
-	UART_SendData(UART1, 'g');
-	while (!UART_GetFlagStatus(UART1, UART_FLAG_TXE)){};
-	UART_SendData(UART1, ':');
-	while (!UART_GetFlagStatus(UART1, UART_FLAG_TXE)){};
+    // 打印提示信息
+	// UART_SendData(UART1, 'g');
+	// while (!UART_GetFlagStatus(UART1, UART_FLAG_TXE)){};
+	// UART_SendData(UART1, ':');
+	// while (!UART_GetFlagStatus(UART1, UART_FLAG_TXE)){};
 	// 1) WHO_AM_I
     uint8_t who = MPU_ReadReg(REG_WHO_AM_I);
 	UART_SendData(UART1, who);
@@ -106,25 +130,11 @@ void gyro_first_read(void)
     // 2) 唤醒（SLEEP=0），最小配置：PWR_MGMT_1 = 0x00
     MPU_WriteReg(REG_PWR_MGMT_1, 0x00);
 
-	delay_ms(10);
+	delay_ms(10);// TODO: 短暂延时等待硬件准备就绪是否必要？
 
     who = MPU_ReadReg(REG_WHO_AM_I);
 	UART_SendData(UART1, who);
 	while (!UART_GetFlagStatus(UART1, UART_FLAG_TXE)){};
-
-	// uint8_t buf[14];
-	// 连续读 14 字节：ACCEL(6) + TEMP(2) + GYRO(6)
-	// MPU_ReadRegs(REG_ACCEL_XOUT_H, buf, 14);
-
-	// int16_t ax = be16_to_i16(buf[0],  buf[1]);
-	// int16_t ay = be16_to_i16(buf[2],  buf[3]);
-	// int16_t az = be16_to_i16(buf[4],  buf[5]);
-	// int16_t t  = be16_to_i16(buf[6],  buf[7]);
-	// int16_t gx = be16_to_i16(buf[8],  buf[9]);
-	// int16_t gy = be16_to_i16(buf[10], buf[11]);
-	// int16_t gz = be16_to_i16(buf[12], buf[13]);
-
-	// LOGF("A[%d,%d,%d]  G[%d,%d,%d]  Traw=%d\r\n", ax, ay, az, gx, gy, gz, t);
 }
 
 // ===== 陀螺仪角度输出（最小可用版：整数近似）=====
